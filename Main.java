6137f9f0bd77b073effa167a82a41b0047ce829f
kimyeon99 <tmdwo7017@naver.com>
2021-03-07 03:22:13 +0900

    meigon program's Main

---
+++ Main.java
@@ -0,0 +1,14 @@
+package program_meigon;
+
+
+public class Main {
+	
+	   final static int PROGRAM_WIDTH = 1280;
+	   final static int PROGRAM_HEIGHT = 720;
+
+
+	public static void main(String[] args) {
+	   new meigon(); 
+	}
+
+}
